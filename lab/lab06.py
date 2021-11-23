66df77c334bf639cd0a8c466de973045e560d90d
jeffli817 <91868131+jeffli817@users.noreply.github.com>
2021-11-23 18:27:34 +0800

    commit lab06

---
+++ lab/lab06.py
@@ -0,0 +1,96 @@
+this_file = __file__
+
+
+def make_adder_inc(a):
+    """
+    >>> adder1 = make_adder_inc(5)
+    >>> adder2 = make_adder_inc(6)
+    >>> adder1(2)
+    7
+    >>> adder1(2) # 5 + 2 + 1
+    8
+    >>> adder1(10) # 5 + 10 + 2
+    17
+    >>> [adder1(x) for x in [1, 2, 3]]
+    [9, 11, 13]
+    >>> adder2(5)
+    11
+    """
+    "*** YOUR CODE HERE ***"
+
+    def add(b):
+        nonlocal a
+        sum = a+b
+        a+=1
+        return sum
+    return add
+
+
+def make_fib():
+    """Returns a function that returns the next Fibonacci number
+    every time it is called.
+
+    >>> fib = make_fib()
+    >>> fib()
+    0
+    >>> fib()
+    1
+    >>> fib()
+    1
+    >>> fib()
+    2
+    >>> fib()
+    3
+    >>> fib2 = make_fib()
+    >>> fib() + sum([fib2() for _ in range(5)])
+    12
+    >>> from construct_check import check
+    >>> # Do not use lists in your implementation
+    >>> check(this_file, 'make_fib', ['List'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    lastTwo = 0
+    lastOne = 0
+    i =0
+    def helper():
+        nonlocal lastTwo
+        nonlocal lastOne
+        nonlocal i
+        if i == 0:
+            i+=1
+            return 0
+        if i == 1:
+            i+=1
+            lastOne = 1
+            return 1
+        sum = lastTwo + lastOne
+        lastTwo = lastOne
+        lastOne = sum
+        return sum
+    return helper
+
+def insert_items(lst, entry, elem):
+    """
+    >>> test_lst = [1, 5, 8, 5, 2, 3]
+    >>> new_lst = insert_items(test_lst, 5, 7)
+    >>> new_lst
+    [1, 5, 7, 8, 5, 7, 2, 3]
+    >>> large_lst = [1, 4, 8]
+    >>> large_lst2 = insert_items(large_lst, 4, 4)
+    >>> large_lst2
+    [1, 4, 4, 8]
+    >>> large_lst3 = insert_items(large_lst2, 4, 6)
+    >>> large_lst3
+    [1, 4, 6, 4, 6, 8]
+    >>> large_lst3 is large_lst
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    listIndex = []
+    for idx,val in enumerate(lst):
+        if val == entry:
+            listIndex.append(idx)
+    for x in reversed(listIndex):
+        lst.insert(x+1,elem)
+    return lst
